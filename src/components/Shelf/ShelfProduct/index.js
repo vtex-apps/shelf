f0b426a63699267cbc6a5dc5e849210da6d6c40a
Daniel Fosco <daniel.fosco@vtex.com.br>
2016-01-11 15:58:01 -0200

    Fix image aspect ratio and shelf heights

--- src/components/Shelf/ShelfProduct/index.js
+++ src/components/Shelf/ShelfProduct/index.js
@@ -21,14 +21,16 @@ class Product extends React.Component {
 
     return (
       <div className="ShelfProduct col-xs-12 col-sm-6 col-md-4 col-lg-4" style={{display: display}}>
-        <Img className="ShelfProduct__photo" src={imageUrl} />
-        <Link to={`/${this.props.slug}/p`} className="ShelfProduct__title row">{name}</Link>
-        <span className="ShelfProduct__price">
+        <div className="ShelfProduct__photo-wrapper"><Img className="ShelfProduct__photo" src={imageUrl} /></div>
+        <div className="ShelfProduct__content">
+          <Link to={`/${this.props.slug}/p`} className="ShelfProduct__title row">{name}</Link>
+          <span className="ShelfProduct__price">
           <Price value={price}/>
-        </span>
-        <button className="ShelfProduct__btn" onTouchTap={this._handleDetails.bind(this)}>
+          </span>
+          <button className="ShelfProduct__btn" onTouchTap={this._handleDetails.bind(this)}>
           Ver detalhes
-        </button>
+          </button>
+        </div>
       </div>
     );
   }
