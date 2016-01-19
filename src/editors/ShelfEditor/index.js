b9c04debf6877064f2c208049acd7ca13723429b
Thor Amorim <thoramorim@gmail.com>
2016-01-19 14:22:36 -0200

    Fix click bug on editor

--- src/editors/ShelfEditor/index.js
+++ src/editors/ShelfEditor/index.js
@@ -8,29 +8,25 @@ class ShelfEditor extends React.Component {
     let settings = this.props.settings;
     if (settings) {
       this.state = settings.toJS();
-      this.state.quantity = this.state.quantity ? this.state.quantity : 1;
+      this.state.quantity = this.state.quantity ? this.state.quantity : 4;
     } else {
       this.state = {
         title: '',
         category: '',
         collection: '',
-        quantity: 1
+        quantity: 4
       };
     }
   }
 
-  minQuantity = 1
-
-  saveSettings = () => {
-    this.props.saveSettings(this.state);
-  }
+  minQuantity = 4
 
   handleSave = (ev) => {
     ev.preventDefault();
-    this.saveSettings();
+    this.props.saveSettings(this.state);
   }
 
-  incrementQuantity(ev) {
+  incrementQuantity = (ev) => {
     ev.preventDefault();
     let newQuantity = parseInt(this.state.quantity);
     this.setState({
@@ -38,25 +34,28 @@ class ShelfEditor extends React.Component {
     });
   }
 
-  decrementQuantity(ev) {
+  decrementQuantity = (ev) => {
     ev.preventDefault();
     let newQuantity = parseInt(this.state.quantity);
+
     if (newQuantity === this.minQuantity) {
       return false;
     }
+
     this.setState({
       quantity: newQuantity - 1
     });
   }
 
-  changeValue(ev) {
+  changeValue = (ev) => {
     this.setState({
       [ev.target.name]: ev.target.value
     });
   }
 
-  changeQuantity(ev) {
+  changeQuantity = (ev) => {
     let value = ev.target.value;
+
     if (isNaN(value) || (value !== '' && parseInt(value) < this.minQuantity)) {
       return false;
     }
@@ -70,33 +69,62 @@ class ShelfEditor extends React.Component {
 
   render() {
     let ActionBar = this.props.actionBar;
+
     return (
       <div className="ShelfEditor">
-        <form className="ShelfEditor__inner" onSubmit={this.handleSubmit.bind(this)}>
+        <form className="ShelfEditor__inner" onSubmit={this.handleSave}>
           <div className="ShelfEditor__text-field">
             <label htmlFor="shelf-title">Título da Prateleira</label>
-            <input id="shelf-title" className="form-control" name="title" type="text"
-                   value={this.state.title} onChange={this.changeValue.bind(this)}
-                   placeholder="Ex: Destaques, Promoção"/>
+            <input
+              id="shelf-title"
+              className="form-control"
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.changeValue}
+              placeholder="Ex: Destaques, Promoção"
+            />
           </div>
           <div className="ShelfEditor__text-field">
             <label htmlFor="shelf-category">Categoria</label>
-            <input id="shelf-category" className="form-control" name="category" type="text"
-                   value={this.state.category} onChange={this.changeValue.bind(this)}
-                   placeholder="Ex: camiseta, cadeira"/>
+            <input
+              id="shelf-category"
+              className="form-control"
+              name="category"
+              type="text"
+              value={this.state.category}
+              onChange={this.changeValue}
+              placeholder="Ex: camiseta, cadeira"
+            />
           </div>
           <div className="ShelfEditor__quant-field">
             <label htmlFor="shelf-quantity">Quantidade de Produtos na Prateleira</label>
             <div className="ShelfEditor__quant-selector">
-              <button className="ShelfEditor__quant-button--left" onTouchTap={this.decrementQuantity.bind(this)}> - </button>
-                <input id="shelf-quantity" className="ShelfEditor__quant-selector__input" name="quantity" type="text"
-                       value={this.state.quantity} onChange={this.changeQuantity.bind(this)}/>
-              <button className="ShelfEditor__quant-button--right" onTouchTap={this.incrementQuantity.bind(this)}> + </button>
+              <button
+                className="ShelfEditor__quant-button--left"
+                onClick={this.decrementQuantity}
+              >
+                -
+              </button>
+              <input
+                id="shelf-quantity"
+                className="ShelfEditor__quant-selector__input"
+                name="quantity"
+                type="text"
+                value={this.state.quantity}
+                onChange={this.changeQuantity}
+              />
+              <button
+                className="ShelfEditor__quant-button--right"
+                onClick={this.incrementQuantity}
+              >
+                +
+              </button>
             </div>
           </div>
         </form>
 
-        <ActionBar onSave={this.handleSave.bind(this)}/>
+        <ActionBar onSave={this.handleSave} />
       </div>
     );
   }
