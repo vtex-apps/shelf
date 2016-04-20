2373d65fbc29d5636e1f8e52cfc1f0bd8e67530b
Thor Amorim <thoramorim@gmail.com>
2016-04-20 19:35:17 -0300

    Update ShelfEditor

--- src/editors/ShelfEditor/ShelfEditor.js
+++ src/editors/ShelfEditor/ShelfEditor.js
@@ -2,86 +2,84 @@ import React from 'react';
 import './ShelfEditor.less';
 
 class ShelfEditor extends React.Component {
-  constructor(props) {
-    super(props);
+  componentWillMount() {
+    const settings = this.props.settings ?
+      this.props.settings.toJS() : null;
 
-    let settings = this.props.settings;
     if (settings) {
-      this.state = settings.toJS();
-      this.state.quantity = this.state.quantity ? this.state.quantity : 4;
+      this.setState({
+        ...settings,
+        quantity: settings.quantity || 4,
+        inputQty: settings.quantity || 4,
+        desktopQty: settings.desktopQty || 4,
+        tabletQty: settings.tabletQty || 2
+      });
     } else {
-      this.state = {
+      this.setState({
         title: '',
         category: '',
         collection: '',
-        quantity: 4
-      };
+        quantity: 4,
+        inputQty: 4,
+        desktopQty: 4,
+        tabletQty: 2
+      });
     }
   }
 
-  minQuantity = 4
-
-  handleSave = (ev) => {
-    ev.preventDefault();
-    this.props.saveSettings(this.state);
+  handleSave = (e) => {
+    e.preventDefault();
+    const { inputQty, ...settings } = this.state;
+    this.props.saveSettings(settings);
   }
 
-  incrementQuantity = (ev) => {
-    ev.preventDefault();
-    let newQuantity = parseInt(this.state.quantity);
-    this.setState({
-      quantity: newQuantity + 1
-    });
-  }
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    let changes = {};
 
-  decrementQuantity = (ev) => {
-    ev.preventDefault();
-    let newQuantity = parseInt(this.state.quantity);
-
-    if (newQuantity === this.minQuantity) {
-      return false;
+    if (name.indexOf('quantity') !== -1) {
+      const intValue = value.length === 0 ? 0 : parseInt(value, 10);
+      value = intValue < 4 ? 4 : intValue;
+      changes.inputQty = e.target.value;
     }
 
-    this.setState({
-      quantity: newQuantity - 1
-    });
-  }
-
-  changeValue = (ev) => {
-    this.setState({
-      [ev.target.name]: ev.target.value
-    });
-  }
-
-  changeQuantity = (ev) => {
-    let value = ev.target.value;
-
-    if (isNaN(value) || (value !== '' && parseInt(value) < this.minQuantity)) {
-      return false;
+    if (name.indexOf('Qty') !== -1) {
+      value = parseInt(value, 10);
     }
 
-    value = value === '' ? value : parseInt(value);
+    changes[name] = value;
+    this.setState(changes);
+  }
 
-    this.setState({
-      [ev.target.name]: value
-    });
+  handleBlur = () => this.setState({ inputQty: this.state.quantity });
+
+  incrementQuantity = (e) => {
+    e.preventDefault();
+    const newQuantity = this.state.quantity + 1;
+    this.setState({ quantity: newQuantity, inputQty: newQuantity });
+  }
+
+  decrementQuantity = (e) => {
+    e.preventDefault();
+    const newQuantity = this.state.quantity - 1;
+    this.setState({ quantity: newQuantity, inputQty: newQuantity });
   }
 
   render() {
-    let ActionBar = this.props.actionBar;
+    const ActionBar = this.props.actionBar;
 
     return (
       <div className="ShelfEditor">
         <form className="ShelfEditor__inner" onSubmit={this.handleSave}>
           <div className="ShelfEditor__text-field">
-            <label htmlFor="shelf-title">Título da Prateleira</label>
+            <label htmlFor="shelf-title">Título</label>
             <input
               id="shelf-title"
               className="form-control"
               name="title"
               type="text"
               value={this.state.title}
-              onChange={this.changeValue}
+              onChange={this.handleChange}
               placeholder="Ex: Destaques, Promoção"
             />
           </div>
@@ -93,16 +91,19 @@ class ShelfEditor extends React.Component {
               name="category"
               type="text"
               value={this.state.category}
-              onChange={this.changeValue}
+              onChange={this.handleChange}
               placeholder="Ex: camiseta, cadeira"
             />
           </div>
           <div className="ShelfEditor__quant-field">
-            <label htmlFor="shelf-quantity">Quantidade de Produtos na Prateleira</label>
+            <label htmlFor="shelf-quantity">
+              Quantidade de produtos total
+            </label>
             <div className="ShelfEditor__quant-selector">
               <button
                 className="ShelfEditor__quant-button--left"
                 onClick={this.decrementQuantity}
+                disabled={this.state.quantity === 4}
               >
                 -
               </button>
@@ -111,8 +112,9 @@ class ShelfEditor extends React.Component {
                 className="ShelfEditor__quant-selector__input"
                 name="quantity"
                 type="text"
-                value={this.state.quantity}
-                onChange={this.changeQuantity}
+                value={this.state.inputQty}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
               />
               <button
                 className="ShelfEditor__quant-button--right"
@@ -120,6 +122,40 @@ class ShelfEditor extends React.Component {
               >
                 +
               </button>
+            </div>
+            <br />
+            <label htmlFor="shelf-quantity-desktop">
+              Quantidade de produtos para exibição (Desktop)
+            </label>
+            <div className="ShelfEditor__quant-selector">
+              <select
+                id="shelf-quantity-desktop"
+                className="form-control"
+                name="desktopQty"
+                value={this.state.desktopQty}
+                onChange={this.handleChange}
+              >
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+            </div>
+            <br />
+            <label htmlFor="shelf-quantity-tablet">
+              Quantidade de produtos para exibição (Tablet)
+            </label>
+            <div className="ShelfEditor__quant-selector">
+              <select
+                id="shelf-quantity-tablet"
+                className="form-control"
+                name="tabletQty"
+                value={this.state.tabletQty}
+                onChange={this.handleChange}
+              >
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
             </div>
           </div>
         </form>
