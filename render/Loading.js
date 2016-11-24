09f141b031d62154556bd912cab701114a827da8
Thor Amorim <thoramorim@gmail.com>
2016-11-24 00:02:17 -0200

    Improve loading to use ShimmerLoading for img

--- render/Loading.js
+++ render/Loading.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react'
-import {CircleLoading, TextBlockLoading} from 'vtex.loading'
+import {ShimmerLoading, TextLineLoading} from 'vtex.loading'
 
 // eslint-disable-next-line
 class Loading extends Component {
   render() {
     return (
       <div className="ph2">
-        <div className="mv6">
-          <CircleLoading />
+        <ShimmerLoading
+          style={{
+            width: '100%',
+            maxWidth: '380px',
+            paddingBottom: '100%',
+          }}
+        />
+        <div className="mt1">
+          <TextLineLoading width="100" />
+          <TextLineLoading width="90" />
         </div>
-        <TextBlockLoading size="small" />
       </div>
     )
   }
