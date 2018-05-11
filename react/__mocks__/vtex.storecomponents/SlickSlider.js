59c4c6c72e7bd849d4fe9c02c36f117ca855f306
Claudivan Filho <claudivan.filho@splab.ufcg.edu.br>
2018-05-11 14:41:03 -0300

    Fix tests

---
+++ react/__mocks__/vtex.storecomponents/SlickSlider.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+/**
+ * SlickSlider Mocked Component.
+ */
+export default class SlickSlider extends Component {
+  render() {
+    return (
+      <div>SlickSlider</div>
+    )
+  }
+}
