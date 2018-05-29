6287572a1e4f31f1c4cefab4581495b1d8b0fa19
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-29 15:36:40 -0300

    ShelfItem propTypes react

--- react/ShelfItem.js
+++ react/ShelfItem.js
@@ -1,66 +1,14 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
 import ProductSummary from 'vtex.product-summary/ProductSummary'
+
+import { shelfItemPropTypes } from './propTypes'
 
 /**
  * ShelfItem Component. Normalizes the item received in the props
  * to adapt to the extension point prop.
  */
 export default class ShelfItem extends Component {
-  static propTypes = {
-    item: PropTypes.shape({
-      productId: PropTypes.string.isRequired,
-      productName: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      categories: PropTypes.array.isRequired,
-      link: PropTypes.string.isRequired,
-      linkText: PropTypes.string.isRequired,
-      brand: PropTypes.string.isRequired,
-      items: PropTypes.arrayOf(
-        PropTypes.shape({
-          itemId: PropTypes.string.isRequired,
-          name: PropTypes.string.isRequired,
-          referenceId: PropTypes.arrayOf(
-            PropTypes.shape({
-              Value: PropTypes.string.isRequired,
-            })
-          ),
-          images: PropTypes.arrayOf(
-            PropTypes.shape({
-              imageUrl: PropTypes.string.isRequired,
-              imageTag: PropTypes.string.isRequired,
-            })
-          ).isRequired,
-          sellers: PropTypes.arrayOf(
-            PropTypes.shape({
-              commertialOffer: PropTypes.shape({
-                /** SKU installments */
-                Installments: PropTypes.arrayOf(
-                  PropTypes.shape({
-                    /** Installment value */
-                    Value: PropTypes.number.isRequired,
-                    /** Interest rate (zero if interest-free) */
-                    InterestRate: PropTypes.number.isRequired,
-                    /** Calculated total value */
-                    TotalValuePlusInterestRate: PropTypes.number,
-                    /** Number of installments */
-                    NumberOfInstallments: PropTypes.number.isRequired,
-                    /** Installments offer name */
-                    Name: PropTypes.string,
-                  })
-                ),
-                Price: PropTypes.number.isRequired,
-                ListPrice: PropTypes.number.isRequired,
-              }).isRequired,
-            })
-          ).isRequired,
-        })
-      ).isRequired,
-    }),
-    extensionId: PropTypes.string.isRequired,
-    summary: PropTypes.any,
-  }
+  static propTypes = shelfItemPropTypes
 
   normalizeProduct(product) {
     if (!product) return null
