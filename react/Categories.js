ae481f0418eead08b2488b136e63353779833b91
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-08-02 15:13:44 -0300

    Fix css top property

--- react/Categories.js
+++ react/Categories.js
@@ -6,7 +6,10 @@ import CategoryCard from './components/CategoryCard'
 
 import './global.css'
 
-const DEFAULT_QUANTITY_ITENS = 4
+const DEFAULT_SLIDES_TO_SHOW = 4
+
+const BREAKPOINT_NOT_LARGE_VIEWPORT = 1080
+const SLIDES_TO_SHOW_NOT_LARGE_VIEWPORT = 2
 
 class Categories extends Component {
   static propTypes = {
@@ -19,12 +22,41 @@ class Categories extends Component {
   }
 
   static defaultProps = {
-    categories: []
+    categories: [
+      {
+        categoryName: 'Sample 1',
+        categoryImageUrl: 'https://li0.rightinthebox.com/images/384x384/201506/ycdgdd1433506208409.jpg'
+      },
+      {
+        categoryName: 'Sample 2',
+        categoryImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY-zF_22MQJt7VAFSH-5g35BiBo8i5ak4uqoCfQAC2PAQvcJU9mg'
+      },
+      {
+        categoryName: 'Sample 3',
+        categoryImageUrl: 'http://d26lpennugtm8s.cloudfront.net/stores/650/612/products/bone-volkswagen-gti-11-8d3565cb88be35b7b315238341270417-640-0.jpg'
+      },
+      {
+        categoryName: 'Sample 4',
+        categoryImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj__Hxl13SZkMM3P0UMLDdi1iddQ_K32LZlO7mG5UYsXfHma_x'
+      },
+      {
+        categoryName: 'Sample 5',
+        categoryImageUrl: 'http://4.bp.blogspot.com/-ItLr46C9rPk/UPk_LUgCvmI/AAAAAAAABmE/crjw9MtL7Ok/s1600/xbox1.jpg'
+      }
+    ]
   }
 
   getSliderSettings = () => {
     return {
-      slidesToShow: DEFAULT_QUANTITY_ITENS
+      slidesToShow: DEFAULT_SLIDES_TO_SHOW,
+      responsive: [
+        {
+          breakpoint: BREAKPOINT_NOT_LARGE_VIEWPORT,
+          settings: {
+            slidesToShow: SLIDES_TO_SHOW_NOT_LARGE_VIEWPORT
+          }
+        }
+      ],
     }
   }
 
