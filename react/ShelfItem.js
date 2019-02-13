01819e809edb4fdb779d60ff53b69ded27b732f3
Gustavo Alves <gustavo.alves@splab.ufcg.edu.br>
2019-02-13 18:45:50 -0300

    Fix undefined possibiliy

--- react/ShelfItem.js
+++ react/ShelfItem.js
@@ -27,7 +27,9 @@ export default class ShelfItem extends Component {
 
   render() {
     const { item, summary } = this.props
-    summary.name.tag = 'h2'
+    if (summary && summary.name) {
+      summary.name.tag = 'h2'
+    }
     return <ProductSummary product={this.normalizeProduct(item)} {...summary} />
   }
 }
