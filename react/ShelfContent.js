386628c837f222961ca6edfa7311e48835939cb3
Theo Moura <theomouram@gmail.com>
2019-03-25 15:50:36 -0300

    Add new slider

--- react/ShelfContent.js
+++ react/ShelfContent.js
@@ -1,172 +1,159 @@
 import PropTypes from 'prop-types'
 import { path } from 'ramda'
 import React, { Component } from 'react'
-import Slider from 'vtex.store-components/Slider'
+import { IconCaret } from 'vtex.store-icons'
+import classNames from 'classnames'
+import { Slider, Slide, Dots, SliderContainer } from 'vtex.slider'
 
 import { getGapPaddingValues } from './paddingEnum'
-
 import ScrollTypes from './ScrollTypes'
 import ShelfItem from './ShelfItem'
 
 import shelf from './shelf.css'
 
+const SLIDER_WIDTH_ONE_ELEMENT = 500
+const SLIDER_WIDTH_TWO_ELEMENTS = 600
+const SLIDER_WIDTH_THREE_ELEMENTS = 900
+const SLIDER_WIDTH_MAX_ELEMENTS = 1300
 const DEFAULT_SHELF_ITEM_WIDTH = 260
-const DOTS_LARGE_VIEWPORT = true
-const SLIDES_TO_SCROLL_LARGE_VIEWPORT = 1
-
-const BREAKPOINT_MOBILE_VIEWPORT = 600
-const SLIDER_CENTER_MOBILE_MODE = true
-const ARROWS_MOBILE_VIEWPORT = false
-const DOTS_MOBILE_VIEWPORT = false
-const DEFAULT_ITEMS_MOBILE = 1
-const DEFAULT_ITEMS_DESKTOP = 3
-const SLIDES_TO_SCROLL_MOBILE_VIEWPORT = 1
-const SLIDES_TO_SHOW_MOBILE_VIEWPORT = 1
-
-const VARIABLE_WIDTH_MOBILE_MODE = true
-
-const BREAKPOINT_EXTRA_SMALL_MOBILE_VIEWPORT = 350
-const DOTS_EXTRA_SMALL_MOBILE_VIEWPORT = true
-const SLIDER_CENTER_MODE_EXTRA_SMALL_MOBILE = true
-
-const ITEMS_TO_FULL_WIDTH = 5
 
 /**
  * ShelfContent Component. Executes the interaction with react-slick
  * and render the properly content of the Shelf depending of edit mode state.
  */
 class ShelfContent extends Component {
-  getSliderSettings = itemsPerPage => {
-    const { arrows } = this.props
-    return {
-      slidesToShow: itemsPerPage,
-      slidesToScroll: SLIDES_TO_SCROLL_LARGE_VIEWPORT,
-      dots: DOTS_LARGE_VIEWPORT,
-      arrows,
-      responsive: [
-        {
-          breakpoint: BREAKPOINT_MOBILE_VIEWPORT,
-          settings: {
-            slidesToShow: SLIDES_TO_SHOW_MOBILE_VIEWPORT,
-            slidesToScroll: SLIDES_TO_SCROLL_MOBILE_VIEWPORT,
-            arrows: ARROWS_MOBILE_VIEWPORT,
-            dots: DOTS_MOBILE_VIEWPORT,
-            centerMode: SLIDER_CENTER_MOBILE_MODE,
-            variableWidth: VARIABLE_WIDTH_MOBILE_MODE,
-          },
-        },
-        {
-          breakpoint: BREAKPOINT_EXTRA_SMALL_MOBILE_VIEWPORT,
-          settings: {
-            slidesToShow: SLIDES_TO_SHOW_MOBILE_VIEWPORT,
-            slidesToScroll: SLIDES_TO_SCROLL_MOBILE_VIEWPORT,
-            arrows: ARROWS_MOBILE_VIEWPORT,
-            dots: DOTS_EXTRA_SMALL_MOBILE_VIEWPORT,
-            centerMode: SLIDER_CENTER_MODE_EXTRA_SMALL_MOBILE,
-            variableWidth: VARIABLE_WIDTH_MOBILE_MODE,
-          },
-        },
-      ],
-    }
+  state = {
+    currentSlide: 0,
   }
 
-  getClassByItemsPerPage(itemsPerPage) {
-    switch (itemsPerPage) {
-      case 5:
-        return 'w-20'
-      case 4:
-        return 'w-25'
-      case 3:
-        return 'w-third'
-    }
+  handleChangeSlide = i => {
+    this.setState({ currentSlide: i })
   }
 
-  get itemsToShow() {
-    const { itemsPerPage, width } = this.props
-    const maxItems = Math.floor(width / DEFAULT_SHELF_ITEM_WIDTH)
-    return maxItems <= itemsPerPage ? maxItems : itemsPerPage
+  handleNextSlide = () => {
+    this.setState(({ currentSlide }) => {
+      const bannersLength = this.props.banners.filter(
+        banner => banner && (banner.mobileImage || banner.image)
+      ).length
+      const nextSlide = (currentSlide + 1) % bannersLength
+
+      return {
+        currentSlide: nextSlide,
+      }
+    })
   }
 
-  get sliderWidth() {
-    const { width } = this.props
-
-    const items = this.itemsToShow
-    const slider = items * DEFAULT_SHELF_ITEM_WIDTH
-
-    if (items >= ITEMS_TO_FULL_WIDTH || width <= slider) return width
-
-    return slider
-  }
-
-  slideFallback = (item = {}, key, fullWidth) => {
-    const { summary, gap } = this.props
-    const style = {
-      width: fullWidth ? '100%' : DEFAULT_SHELF_ITEM_WIDTH,
-    }
+  arrowRender = ({ orientation, onClick }) => {
+    const containerClasses = classNames(
+      shelf.arrow,
+      'pointer z-1 flex absolute',
+      {
+        [shelf.arrowLeft]: orientation === 'left',
+        [shelf.arrowRight]: orientation === 'right',
+      }
+    )
     return (
-      <div key={key} className={`${shelf.slide} h-100`}>
-        <div style={style} className={`${gap} h-100`}>
-          <ShelfItem item={item} summary={summary} />
-        </div>
+      <div className={containerClasses} onClick={onClick}>
+        <IconCaret orientation={orientation} thin size={25} />
       </div>
     )
   }
 
-  ssrFallback() {
-    const { products, isMobile } = this.props
-    const numberOfItems = isMobile
-      ? DEFAULT_ITEMS_MOBILE
-      : DEFAULT_ITEMS_DESKTOP
-    const className = this.getClassByItemsPerPage(numberOfItems)
+  ArrowContainerRender = ({ children }) => {
+    const wrapperClasses = classNames(
+      shelf.arrowsContainerWrapper,
+      'w-100 h-100 absolute left-0 top-0 flex justify-center'
+    )
+    const containerClasses = classNames(
+      shelf.arrowsContainer,
+      'w-100 h-100 mw9 flex-ns justify-between items-center dn-s'
+    )
+
     return (
-      <div className="flex justify-center">
-        {products &&
-          products.slice(0, numberOfItems).map((item, index) => {
-            return (
-              <div
-                key={item.productId}
-                className={`${className} flex justify-center`}
-              >
-                {this.slideFallback(item, path(['productId'], item) || index)}
-              </div>
-            )
-          })}
+      <div className={wrapperClasses}>
+        <Container className={containerClasses}>{children}</Container>
+      </div>
+    )
+  }
+
+  slideFallback = (item = {}, key, fullWidth) => {
+    const { summary } = this.props
+    const style = {
+      width: fullWidth ? '100%' : DEFAULT_SHELF_ITEM_WIDTH,
+    }
+    return (
+      <div key={key} style={style} className="h-100">
+        <ShelfItem item={item} summary={summary} />
       </div>
     )
   }
 
   render() {
-    const { products, maxItems, scroll, isMobile, width } = this.props
+    const {
+      products,
+      maxItems,
+      scroll,
+      gap,
+      width,
+      itemsPerPage,
+      arrows,
+    } = this.props
+
     const isScrollByPage = scroll === ScrollTypes.BY_PAGE.value
-    const sliderSettings = this.getSliderSettings(this.itemsToShow)
-    const sliderWidth =
-      isMobile || width <= BREAKPOINT_MOBILE_VIEWPORT ? width : this.sliderWidth
-    const styles = {
-      width: sliderWidth,
-    }
+
     const isFullWidth = width === this.sliderWidth
     const productList =
       !products || !products.length ? Array(maxItems).fill(null) : products
+
+    const perPage = {
+      [SLIDER_WIDTH_MAX_ELEMENTS]: itemsPerPage,
+      [SLIDER_WIDTH_THREE_ELEMENTS]: 3,
+      [SLIDER_WIDTH_TWO_ELEMENTS]: 2,
+      [SLIDER_WIDTH_ONE_ELEMENT]: 1,
+    }
+
+    const { currentSlide } = this.state
+
     return (
-      <div className="vtex-shelf__content flex justify-center">
-        <div className="mw9" style={styles}>
+      <div className="vtex-shelf__content flex justify-center ph5">
+        <SliderContainer className="w-100 mw9 pv5">
           <Slider
-            ssrFallback={this.ssrFallback()}
-            sliderSettings={sliderSettings}
+            perPage={perPage}
+            onChangeSlide={this.handleChangeSlide}
+            currentSlide={currentSlide}
+            arrowRender={arrows && this.arrowRender}
             scrollByPage={isScrollByPage}
-            defaultItemWidth={DEFAULT_SHELF_ITEM_WIDTH}
+            loop
           >
-            {productList
-              .slice(0, maxItems)
-              .map((item, index) =>
-                this.slideFallback(
+            {productList.slice(0, maxItems).map((item, index) => (
+              <Slide
+                className={classNames('justify-center', gap)}
+                key={path(['productId'], item) || index}
+                defaultWidth={DEFAULT_SHELF_ITEM_WIDTH}
+              >
+                {this.slideFallback(
                   item,
                   path(['productId'], item) || index,
                   isFullWidth
-                )
-              )}
+                )}
+              </Slide>
+            ))}
           </Slider>
-        </div>
+          <Dots
+            loop
+            showDotsPerPage
+            perPage={perPage}
+            currentSlide={currentSlide}
+            totalSlides={products.length}
+            onChangeSlide={this.handleChangeSlide}
+            classes={{
+              root: 'pt4',
+              notActiveDot: 'bg-muted-3',
+              dot: classNames(shelf.dot, 'mh2 mv0 pointer br-100'),
+              activeDot: 'bg-emphasis',
+            }}
+          />
+        </SliderContainer>
       </div>
     )
   }
