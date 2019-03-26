7a1c175340690cde57bac808296059895a488330
Theo Moura <theomouram@gmail.com>
2019-03-26 17:33:27 -0300

    Fix gap between itens

--- react/ShelfContent.js
+++ react/ShelfContent.js
@@ -1,33 +1,75 @@
 import PropTypes from 'prop-types'
-import { path } from 'ramda'
+import { path, range } from 'ramda'
 import React, { Component } from 'react'
 import { IconCaret } from 'vtex.store-icons'
 import classNames from 'classnames'
-import { Slider, Slide, Dots, SliderContainer } from 'vtex.slider'
+import {
+  Slider,
+  Slide,
+  Dots,
+  SliderContainer,
+  resolveSlidesNumber,
+} from 'vtex.slider'
 
 import { getGapPaddingValues } from './paddingEnum'
 import ScrollTypes from './ScrollTypes'
 import ShelfItem from './ShelfItem'
 
 import shelf from './shelf.css'
+import { forEach } from 'iterall'
 
-const SLIDER_WIDTH_ONE_ELEMENT = 500
-const SLIDER_WIDTH_TWO_ELEMENTS = 600
-const SLIDER_WIDTH_THREE_ELEMENTS = 900
-const SLIDER_WIDTH_MAX_ELEMENTS = 1300
+const SLIDER_WIDTH_ONE_ELEMENT = 320
+const SLIDER_WIDTH_TWO_ELEMENTS = 500
+const SLIDER_WIDTH_THREE_ELEMENTS = 750
+const SLIDER_WIDTH_FIVE_ELEMENTS = 1290
+const SLIDER_WIDTH_FOUR_ELEMENTS = 1000
 const DEFAULT_SHELF_ITEM_WIDTH = 260
+const ITEMS_TO_FULL_WIDTH = 5
 
 /**
  * ShelfContent Component. Executes the interaction with react-slick
  * and render the properly content of the Shelf depending of edit mode state.
  */
 class ShelfContent extends Component {
-  state = {
-    currentSlide: 0,
+  constructor(props) {
+    super(props)
+    this.perPage = {
+      [SLIDER_WIDTH_FIVE_ELEMENTS]: 5,
+      [SLIDER_WIDTH_FOUR_ELEMENTS]: 4,
+      [SLIDER_WIDTH_THREE_ELEMENTS]: 3,
+      [SLIDER_WIDTH_TWO_ELEMENTS]: 2,
+      [SLIDER_WIDTH_ONE_ELEMENT]: 1,
+    }
+    this.state = {
+      currentSlide: 0,
+      firstRender: true,
+    }
   }
+
+  // calcPerPage = () => {
+  //   const { itemsPerPage } = this.props
+  //   range(1, itemsPerPage + 1).forEach(index => {
+  //     this.perPage[index] = index
+  //   })
+  // }
 
   handleChangeSlide = i => {
     this.setState({ currentSlide: i })
+  }
+
+  get sliderWidth() {
+    const { width } = this.props
+
+    const items = resolveSlidesNumber(this.perPage)
+    const slider = items * DEFAULT_SHELF_ITEM_WIDTH
+
+    if (items >= ITEMS_TO_FULL_WIDTH || width <= slider) return width
+
+    return slider
+  }
+
+  componentDidMount() {
+    this.setState({ firstRender: false })
   }
 
   arrowRender = ({ orientation, onClick }) => {
@@ -35,8 +77,8 @@ class ShelfContent extends Component {
       shelf.arrow,
       'pointer z-1 flex absolute',
       {
-        [shelf.arrowLeft]: orientation === 'left',
-        [shelf.arrowRight]: orientation === 'right',
+        [`${shelf.arrowLeft} left-0`]: orientation === 'left',
+        [`${shelf.arrowRight} right-0`]: orientation === 'right',
       }
     )
     return (
@@ -70,61 +112,54 @@ class ShelfContent extends Component {
       scroll,
       gap,
       width,
-      itemsPerPage,
       arrows,
       summary,
     } = this.props
+
+    const { firstRender } = this.state
 
     const isScrollByPage = scroll === ScrollTypes.BY_PAGE.value
 
     const isFullWidth = width === this.sliderWidth
 
     const style = {
-      width: isFullWidth ? '100%' : DEFAULT_SHELF_ITEM_WIDTH,
+      width: '100%',
     }
     const productList =
       !products || !products.length ? Array(maxItems).fill(null) : products
 
-    const perPage = {
-      [SLIDER_WIDTH_MAX_ELEMENTS]: itemsPerPage,
-      [SLIDER_WIDTH_THREE_ELEMENTS]: 3,
-      [SLIDER_WIDTH_TWO_ELEMENTS]: 2,
-      [SLIDER_WIDTH_ONE_ELEMENT]: 1,
-    }
-
     const { currentSlide } = this.state
 
     return (
-      <div className="vtex-shelf__content flex justify-center ph5">
-        <SliderContainer className="w-100 mw9 pv5">
+      <div className="vtex-shelf__content flex justify-center">
+        <SliderContainer
+          style={!firstRender ? { width: this.sliderWidth } : {}}
+          className="mw9"
+        >
           <Slider
-            perPage={perPage}
+            perPage={this.perPage}
             onChangeSlide={this.handleChangeSlide}
             currentSlide={currentSlide}
             arrowRender={arrows && this.arrowRender}
             scrollByPage={isScrollByPage}
+            duration={400}
             loop
           >
             {productList.slice(0, maxItems).map((item, index) => (
               <Slide
-                className={classNames('justify-center', gap)}
+                sliderTransitionDuration={400}
+                className={classNames('justify-center h-100', gap)}
                 key={path(['productId'], item) || index}
                 defaultWidth={DEFAULT_SHELF_ITEM_WIDTH}
               >
-                <div
-                  key={path(['productId'], item) || index}
-                  style={style}
-                  className="h-100"
-                >
-                  <ShelfItem item={item} summary={summary} />
-                </div>
+                <ShelfItem item={item} summary={summary} />
               </Slide>
             ))}
           </Slider>
           <Dots
             loop
             showDotsPerPage
-            perPage={perPage}
+            perPage={this.perPage}
             currentSlide={currentSlide}
             totalSlides={products.length}
             onChangeSlide={this.handleChangeSlide}
