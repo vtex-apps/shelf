7c75fee67c513e4d7f1139c0aeba5ba73be7bed1
Joao Fidelis <joaogfidelis@gmail.com>
2019-05-24 15:29:02 -0300

    add more crossSelling options, make related prod shelf make query

--- react/RelatedProducts.js
+++ react/RelatedProducts.js
@@ -1,89 +1,116 @@
+import React from 'react'
 import PropTypes from 'prop-types'
-import { path } from 'ramda'
-import React, { Component } from 'react'
+import { path, last } from 'ramda'
+import { Query } from 'react-apollo'
+
+import productRecommendations from './queries/productRecommendations.gql'
 
 import ProductList from './ProductList'
 import { productListSchemaPropTypes } from './propTypes'
-import { shelfItemPropTypes } from './propTypes'
 import shelf from './shelf.css'
+
+
+// Previous values were in a wrong format with the message string in the enum value.
+const fixRecommendation = recommendation => {
+  if (recommendation.includes('admin/editor.relatedProducts.')) {
+    return last(recommendation.split('.'))
+  }
+  return recommendation
+}
 
 /**
  * Related Products Component. Queries and shows the related products
  */
-export default class RelatedProducts extends Component {
-  static propTypes = {
-    /** Main product to have related products queried */
-    slug: PropTypes.string,
-    /** Graphql productQuery response. */
-    productQuery: PropTypes.shape({
-      /** Product to have related products queried */
-      product: PropTypes.shape({
-        /** Recommendations property */
-        recommendations: PropTypes.shape({
-          /** View recommendations (who saw this product, also saw...) */
-          view: PropTypes.arrayOf(shelfItemPropTypes.item),
-          /** Buy recommendations (who bought this product, also bought...) */
-          buy: PropTypes.arrayOf(shelfItemPropTypes.item),
-          /** Similar products */
-          similars: PropTypes.arrayOf(shelfItemPropTypes.item),
-        }),
-      }),
-    }),
-    /** ProductList schema configuration */
-    productList: PropTypes.shape(productListSchemaPropTypes),
-  }
 
-  static defaultProps = {
-    recommendation: 'admin/editor.relatedProducts.similars',
-    productList: {
-      ...ProductList.defaultProps,
-      titleText: 'Related Products',
+const RelatedProducts = ({ productQuery, productList, recommendation: cmsRecommendation }) => {
+  const productId = path(['product', 'productId'], productQuery)
+  if (!productId) {
+    return null
+  }
+  const recommendation = fixRecommendation(cmsRecommendation)
+  const variables = { identifier: { field: 'id', value: productId }, type: recommendation }
+  return (
+    <Query 
+      query={productRecommendations}
+      variables={variables}
+      partialRefetch
+      ssr={false}
+    > 
+    {({data, loading}) => {
+      const { productRecommendations } = data
+      const productListProps = {
+        products: productRecommendations || [],
+        loading,
+        ...productList,
+      }
+      return (
+        <div className={shelf.relatedProducts}>
+          <ProductList {...productListProps} />
+        </div>
+      )
+    }}
+    </Query>
+  )
+}
+
+RelatedProducts.propTypes = {
+  /** Main product to have related products queried */
+  slug: PropTypes.string,
+  /** Graphql productQuery response. */
+  productQuery: PropTypes.shape({
+    /** Product to have related products queried */
+    product: PropTypes.shape({
+      productId: PropTypes.string,
+    }),
+    loading: PropTypes.bool,
+  }),
+  /** ProductList schema configuration */
+  productList: PropTypes.shape(productListSchemaPropTypes),
+}
+
+RelatedProducts.defaultProps = { 
+  recommendation: 'similars',
+  productList: {
+    ...ProductList.defaultProps,
+    titleText: 'Related Products',
+  },
+}
+
+RelatedProducts.getSchema = props => {
+  const productListSchema = ProductList.getSchema(props)
+  productListSchema.properties.titleText.default =
+    RelatedProducts.defaultProps.productList.titleText
+
+  return {
+    title: 'admin/editor.relatedProducts.title',
+    description: 'admin/editor.relatedProducts.description',
+    type: 'object',
+    properties: {
+      recommendation: {
+        title: 'admin/editor.relatedProducts.recommendation',
+        description: 'admin/editor.relatedProducts.recommendation.description',
+        type: 'string',
+        default: RelatedProducts.defaultProps.recommendation,
+        enum: [
+          'similars',
+          'view',
+          'buy',
+          'accessories',
+          'viewAndBought',
+          'suggestions'
+        ],
+        enumNames: [
+          'admin/editor.relatedProducts.similars',
+          'admin/editor.relatedProducts.view',
+          'admin/editor.relatedProducts.buy',
+          'admin/editor.relatedProducts.accessories',
+          'admin/editor.relatedProducts.viewAndBought',
+          'admin/editor.relatedProducts.suggestions',
+        ],
+      },
+      productList: productListSchema,
     },
   }
-
-  static getSchema = props => {
-    const productListSchema = ProductList.getSchema(props)
-    productListSchema.properties.titleText.default =
-      RelatedProducts.defaultProps.productList.titleText
-
-    return {
-      title: 'admin/editor.relatedProducts.title',
-      description: 'admin/editor.relatedProducts.description',
-      type: 'object',
-      properties: {
-        recommendation: {
-          title: 'admin/editor.relatedProducts.recommendation',
-          description: 'admin/editor.relatedProducts.recommendation.description',
-          type: 'string',
-          default: RelatedProducts.defaultProps.recommendation,
-          enum: [
-            'admin/editor.relatedProducts.similars',
-            'admin/editor.relatedProducts.view',
-            'admin/editor.relatedProducts.buy',
-          ],
-        },
-        productList: productListSchema,
-      },
-    }
-  }
-
-  render() {
-    const { productQuery, productList } = this.props
-    const recommendation = this.props.recommendation.split('.').pop()
-    const products = path(
-      ['product', 'recommendations', recommendation],
-      productQuery
-    )
-    const productListProps = {
-      products,
-      loading: productQuery.loading,
-      ...productList,
-    }
-
-    return (
-      <div className={shelf.relatedProducts}>
-        <ProductList {...productListProps} />
-      </div>
-    )
-  }
 }
+
+export default RelatedProducts
