9fa96100431ec380cd6b98d17a6dcf0260ded479
Breno Calazans <brenocalazans@gmail.com>
2019-05-25 18:19:07 -0300

    Fix tests

--- react/__mocks__/vtex.pixel-manager/PixelContext.js
+++ react/__mocks__/vtex.pixel-manager/PixelContext.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-export function Pixel(Comp) {
-  return class extends React.Component {
-    render() {
-      return <Comp {...this.props} push={() => {}} subscribe={() => {}} />
-    }
-  }
+export function usePixel() {
+  return { push: () => null }
 }
